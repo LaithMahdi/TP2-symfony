c76153f51ef9a6042197076bd440b31a1e45a7a1
Mahdi Laith <mahdilaith380@gmail.com>
2023-03-02 15:29:42 +0100

    first commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+Use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/{name}', name: 'Index',methods:['GET','HEAD'])]
+    public function index($name):Response
+    {
+    
+       return $this->render("index.html.twig",['name'=>$name]);
+    }
+
+}
+
+?>
